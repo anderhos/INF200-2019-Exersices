c354ccc4936fba1ad4bd9717ee49bbc627b19ee5
Helge <helge.helo@gmail.com>
2019-10-03 15:19:49 +0200

    Added median function and imported pytest

--- src/helge_helo_klemetsdal_ex/ex03/test_median.py
+++ src/helge_helo_klemetsdal_ex/ex03/test_median.py
@@ -2,3 +2,23 @@
 
 __author__ = 'Helge Helo Klemetsdal'
 __email__ = 'hegkleme@nmbu.no'
+
+import pytest
+
+
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sort_data = sorted(data)
+    num_el = len(sort_data)
+    if num_el == 0:
+        raise ValueError
+    if num_el % 2 == 1:
+        return sort_data[num_el // 2]
+    else:
+        return (sort_data[num_el // 2 - 1] + sort_data[num_el // 2]) / 2
