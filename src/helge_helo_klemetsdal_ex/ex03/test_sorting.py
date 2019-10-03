a11d5c1abf26300ca3ea69802897a4901690bc88
Helge <helge.helo@gmail.com>
2019-10-03 15:06:20 +0200

    Added test_empty() function and bubble_sort()

--- src/helge_helo_klemetsdal_ex/ex03/test_sorting.py
+++ src/helge_helo_klemetsdal_ex/ex03/test_sorting.py
@@ -2,3 +2,20 @@
 
 __author__ = 'Helge Helo Klemetsdal'
 __email__ = 'hegkleme@nmbu.no'
+
+
+def bubble_sort(somedata):
+    dat = list(somedata)
+    data_length = len(dat)
+    while data_length > 1:
+        for index in range(data_length - 1):
+            if dat[index] > dat[index + 1]:
+                dat[index], dat[index + 1] = dat[index + 1], dat[index]
+        data_length -= 1
+    return dat
+
+
+
+def test_empty():
+    """Test that the sorting function works for empty list"""
+    assert bubble_sort([]) == []
