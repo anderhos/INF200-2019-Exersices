d5ee2cc11855b5fc803f3f55e95597dea0a69e5b
Anders Mølmen Høst <anderhos@nmbu.no>
2019-10-15 14:41:09 +0200

    updated name labels

--- src/helge_helo_klemetsdal_ex/pa01/chutes.py
+++ src/helge_helo_klemetsdal_ex/pa01/chutes.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
 __author__ = 'Helge Helo Klemetsdal' 'Anders Mølmen Høst'
 __email__ = 'hegkleme@nmbu.no' 'anderhos@nmbu.no'
-=======
-__author__ = 'Helge Helo Klemetsdal'
-__email__ = 'hegkleme@nmbu.no'
->>>>>>> pa01
+
